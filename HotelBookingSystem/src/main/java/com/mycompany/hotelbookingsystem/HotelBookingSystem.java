7e5fdecd0850953c19c541f725f05dd3e264f726
yossef-seyam <yoyoseyam12345@gmail.com>
2025-04-29 13:16:06 +0300

    test

--- HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/HotelBookingSystem.java
+++ HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/HotelBookingSystem.java
@@ -11,6 +11,6 @@ package com.mycompany.hotelbookingsystem;
 public class HotelBookingSystem {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Seyam");
     }
 }
