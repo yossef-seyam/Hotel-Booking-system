b3415a983ab17adb1ac563481050e85b88b70076
dell <dell@DESKTOP-CN02UDN>
2025-04-30 00:32:50 +0300

    shehata

--- HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/HotelBookingSystem.java
+++ HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/HotelBookingSystem.java
@@ -11,6 +11,6 @@ package com.mycompany.hotelbookingsystem;
 public class HotelBookingSystem {
 
     public static void main(String[] args) {
-        System.out.println("7MKSH");
+        System.out.println("shehata");
     }
 }
