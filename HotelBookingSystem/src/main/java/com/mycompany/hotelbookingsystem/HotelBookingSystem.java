86794968ab65b4f1eb2dcbcadf786490e214dbbb
yossef-seyam <yoyoseyam12345@gmail.com>
2025-04-29 13:08:52 +0300

    Initial NetBeans project structure

---
+++ HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/HotelBookingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelbookingsystem;
+
+/**
+ *
+ * @author user
+ */
+public class HotelBookingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
