0f615e1d369e0a863eba03a77cb862a604857e53
yossef-seyam <yoyoseyam12345@gmail.com>
2025-05-03 01:32:30 +0300

    deleted customer

--- HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/Customer.java
+++ HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/Customer.java
@@ -10,21 +10,11 @@ package com.mycompany.hotelbookingsystem;
  */
 public class Customer {
     /****** Data Fields ******/
-    static int customerID;
-    String customerName;
-    String customerEmail;
+    
     
     /****** Constructor ******/
-    public Customer(String customerName, String customerEmail){
-        this.customerName = customerName;
-        this.customerEmail = customerEmail;
-        customerID++;
-    }
+   
 
     /****** Methods ******/
-    void displayInfo() {
-        System.out.println(" Customer Name : " + customerName);
-        System.out.println(" Customer Email : "+ customerEmail);
-        System.out.println(" Customer ID : " + customerID);
-    }
+    
 }
