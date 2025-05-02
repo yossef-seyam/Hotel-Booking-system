57553f3b8371b9d94a9ea7980624b64a8717b894
yossef-seyam <yoyoseyam12345@gmail.com>
2025-05-03 01:20:30 +0300

    deleted room class

--- HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/Room.java
+++ HotelBookingSystem/src/main/java/com/mycompany/hotelbookingsystem/Room.java
@@ -11,22 +11,15 @@ package com.mycompany.hotelbookingsystem;
 public abstract class Room {
     
     /****** Data Fields ******/
-    protected double price;
-    protected String roomType; // ايه لازمته مش فاهم
-    protected static int roomID;
-    protected boolean isAvailable ;
+    
     /****** Constructor ******/
     
-    Room(double price){
-        this.price = price;
-    }
     
     /****** Methods ******/
     abstract void displayInfo();
     
-    
-    
 }
+
 class SingleRoom extends Room{
     
     /****** Data Fields ******/
@@ -34,51 +27,20 @@ class SingleRoom extends Room{
     
     /****** Constructor ******/
     
-    SingleRoom(double price){
-        super(price);
-        super.roomType = "Single Room";
-        isAvailable = true;
-        roomID++;
-    }
+    
     
     
     /****** Methods ******/
-    @Override
-    void displayInfo(){
-        System.out.println(" Room type : Single Room ");
-        System.out.println(" Room price : 100$ ");
-        System.out.println(" Room ID : " + roomID);
-        
-    }
-    @Override
-    public String toString() {
-    return "Room type : " + super.roomType + ", Price : $" + price ;
-    }
+    
     
 }
 class DoubleRoom extends Room{
     /****** Data Fields ******/
     
     /****** Constructor ******/
-    public DoubleRoom(double price){
-        super(price);
-        super.roomType = "Double Room";
-        isAvailable = true;
-        roomID++;
-        
-    }
-
+    
     /****** Methods ******/
-    @Override
-            void displayInfo() {
-                System.out.println(" Room type : Double Room ");
-                System.out.println(" Room price : 200$ ");
-                System.out.println(" Room ID : " + roomID);
-    }
-    @Override
-    public String toString() {
-    return "Room type : " + super.roomType + ", Price : $" + price ;
-    }
+    
 }
     
     
@@ -87,24 +49,10 @@ class SuiteRoom extends Room{
     
     /****** Constructor ******/
     
-    SuiteRoom(double price){
-        super(price);
-        super.roomType = "Suite Room";
-        isAvailable = true;
-        roomID++;
-    }
+    
     
     
     
     /****** Methods ******/
-    @Override
-    void displayInfo(){
-        System.out.println(" Room type : Suite Room ");
-        System.out.println(" Room price : 500$ ");
-        System.out.println(" Room ID : " + roomID);
-    }
-    @Override
-    public String toString() {
-    return "Room type : " + super.roomType + ", Price : $" + price ;
-    }
+    
 }
