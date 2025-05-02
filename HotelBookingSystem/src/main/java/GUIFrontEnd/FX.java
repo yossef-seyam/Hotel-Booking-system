50c64e92973cd1f31d71f8c54fd8d064644c19dc
yossef-seyam <yoyoseyam12345@gmail.com>
2025-05-02 15:27:16 +0300

    added Front End file

---
+++ HotelBookingSystem/src/main/java/GUIFrontEnd/FX.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUIFrontEnd;
+/*
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+*/
+
+/**
+ *
+ * @author user
+ */
+public class FX {
+    
+}
